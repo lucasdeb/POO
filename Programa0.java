88453f22e24611581ee54412c73273e11b10baa7
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2023-08-18 16:26:39 -0300

    first file

---
+++ Programa0.java
@@ -0,0 +1,16 @@
+public class Programa0{
+    public static void main(string[] args){
+
+        int data = 10;
+        char letra;
+        boolean b = true;
+        float num = 10.6f;
+        byte dato1 = 43;
+        short num1 = 100;
+        long num2 = 92837982735;
+
+
+
+        System.out.println(data);
+    }
+}

ee9d53dd7e4875ffebd87e6ad78144facb9a755b
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2023-08-23 10:00:15 -0300

    Try catch

--- Programa0.java
+++ Programa0.java
@@ -8,7 +8,7 @@ public class Programa0{
         byte dato1 = 43;
         short num2 = 100;
 
-
+        //Vectors (arrays) practice
 
         System.out.println(num1);
 
@@ -16,6 +16,19 @@ public class Programa0{
 
         for(int num : array){
             System.out.println(num);
+        }
+
+        //Try catch exceptions
+
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+            } 
+            catch (Exception i) {
+            System.out.println("Something went wrong.");
+            } 
+            finally {
+            System.out.println("The 'try catch' is finished.");
         }
     }
 }
